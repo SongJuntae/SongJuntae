f4ce076cb515d597e1ac6f46e3f81fe0895df890
SongJuntae <sjt0332@likelion.org>
2022-12-27 17:50:47 +0900

    work

--- script/external_javascript.js
+++ script/external_javascript.js
@@ -1,4 +1,7 @@
 // ========================================================================================================================
 // 내가 만든 javascript
-
+function Theme() {
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
 //
