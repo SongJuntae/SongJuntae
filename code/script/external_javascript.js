8e128a80bd88bd4683225726815e05cd3a648ca3
SongJuntae <sjt0332@likelion.org>
2023-01-03 16:51:25 +0900

    work

--- code/script/external_javascript.js
+++ code/script/external_javascript.js
@@ -6,18 +6,31 @@ function Theme() {
 }
 // ======================================================================================================================== 낫 모드 토글 끝
 // ======================================================================================================================== 글목록 시작
+function fetchPost(identifier) {
+    fetch(identifier).then(function (reponse) {
+        reponse.text().then(function (text) {
+            const divpost = document.querySelector("#post");
+            const post = document.createElement("article");
+            post.innerHTML = text;
+            divpost.appendChild(post)
+        })
+    })
+}
+
 function fetchListOfPosts(url) {
     fetch(url).then(function (reponse) {
         reponse.json().then(function (text) {
             for (var i in [...Array(text['data'].length).keys()]) {
                 const ol = document.querySelector("ol");
                 const li = document.createElement("li");
-                li.textContent = text['data'][i]['title'] + ' ' + text['data'][i]['creator'] + ' ' + text['data'][i]['date'];
+                li.innerHTML = '<a onclick="fetchPost(\'' + text['data'][i]['identifier'] + '\')">' + text['data'][i]['title'] + ' ' + text['data'][i]['creator'] + ' ' + text['data'][i]['date'] + '</a>';
                 ol.appendChild(li);
             }
         })
     })
 }
+
+
 // ======================================================================================================================== 글목록 끝
 // ======================================================================================================================== test 시작
 // '<pre>' + text['data'][0]['title'] + '&nbsp' + text['data'][0]['creator'] + '&nbsp' + text['data'][0]['date'] + '</pre>';
